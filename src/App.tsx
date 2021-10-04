89876c10c39e91239b7dca6c8698d6fc03783e24
Philipe Faria <philipefaria61@gmail.com>
2021-10-03 21:24:21 -0300

    Modal structure completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
-import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import { createServer } from 'miragejs';
+import Modal from 'react-modal';
 
+
+import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+        },
+      ];
+    });
+  }
+});
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);  
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+ 
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />      
+
       <GlobalStyle />
     </>
   );
